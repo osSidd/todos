aa09862e8e08b3feae8f5bcc0b2e21c310a4aadb
Osama <107386980+osSidd@users.noreply.github.com>
2023-11-01 21:53:15 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css'
+
+export default function App(){
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>
+                lorem ipsum dolor sit amet consectur...
+            </p>
+            
+        </div>
+    )
+}
